d614fd74da906ced87040dfc4465b5f564618d3c
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-06-06 21:23:27 -0700

    feat: create kombatant generator and update styles

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
-const root = document.querySelector("#app");
+const kombatants = [
+  "rambo",
+  "rain",
+  "mileena",
+  "robocop",
+  "sheeva",
+  "fujin",
+  "spawn",
+  "the-joker",
+  "jade",
+  "erron-black",
+  "kabal",
+  "kung-lao",
+  "sub-zero",
+  "scorpion",
+  "cetrion",
+  "frost",
+  "baraka",
+  "raiden",
+  "shao-kahn",
+  "d-vorah",
+  "jax-briggs",
+  "geras",
+  "kano",
+  "terminator",
+  "nightwolf",
+  "cassie-cage",
+  "kotal-kahn",
+  "skarlet",
+  "sonya-blade",
+  "shang-tsung",
+  "johnny-cage",
+  "noob-saibot",
+  "kollector",
+  "kitana",
+  "jacqui-briggs",
+  "liu-kang",
+  "sindel",
+];
 
-root.innerHTML("For the neatherrealm!");
+function generateKombatants(kombatantsArr) {
+  const characterPool = document.querySelector("#character-pool");
+
+  kombatantsArr.map((kombatant) => {
+    const kombatantImg = document.createElement("img");
+    kombatantImg.setAttribute(
+      "src",
+      `https://cdn-prod.mortalkombat.com/roster/${kombatant}/thumb-p.png`
+    );
+    kombatantImg.setAttribute("alt", kombatant);
+    kombatantImg.setAttribute("id", kombatant);
+    kombatantImg.setAttribute("class", "character-img");
+    kombatantImg.setAttribute("draggable", "true");
+
+    characterPool.appendChild(kombatantImg);
+  });
+}
+
+generateKombatants(kombatants);
